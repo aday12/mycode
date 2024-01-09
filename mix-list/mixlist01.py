606fda10ff1416da858db1c544f7b4f6e517b56c
aday104 <aaronday1996@icloud.com>
2024-01-09 23:10:50 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# create a list
+my_list = [ "192.168.0.5", 5060, "UP" ]
+# print the first item
+print("The first item in the list (IP): " + my_list[0] )
+# print second item
+print("The second item in the list (port): " + str(my_list[1]) )
+# print third item
+print("The last item in the list (state): " + my_list[2] )
+
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print("IP Addresses: " + iplist[3] + ", and " + iplist[4] )
