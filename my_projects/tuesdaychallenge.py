70c8d4d344b6fca2eb05de4cd4b10cc80389e6be
aday104 <aaronday1996@icloud.com>
2024-01-10 16:38:31 +0000

    tues challenge improved

--- my_projects/tuesdaychallenge.py
+++ my_projects/tuesdaychallenge.py
@@ -6,5 +6,5 @@ name = input("What is your name?").strip().capitalize()
 
 # This is what you should see when print runs-
 # Hi <name>! Welcome to Day 2 of Python Training!
-print("\nHi " + name.capitalize() + "! Welcome to Day " + str(mylist[1]) + " of " + str(mylist[5]) + " Training!")
+print("\nHi " + name + "! Welcome to Day " + str(mylist[1]) + " of " + str(mylist[5]) + " Training!")
 
