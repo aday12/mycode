36e3f1f78e1fa64d10c5b99e2300dabf82e7c7dd
aday104 <aaronday1996@icloud.com>
2024-01-10 16:34:42 +0000

    tuesday_challenge

--- my_projects/tuesdaychallenge.py
+++ my_projects/tuesdaychallenge.py
@@ -1,10 +1,10 @@
-#!/usr/env/python3
+#!/usr/bin/env python3
 
-mylist= [1,2,3,4,5, "Python"]
+mylist = [1, 2, 3, 4, 5, "Python"]
 
-name=input("What is your name?\n")
+name = input("What is your name?").strip().capitalize()
 
 # This is what you should see when print runs-
 # Hi <name>! Welcome to Day 2 of Python Training!
-print("Hi " + name.capitalize + "! Welcome to Day " + mylist[1] + " of " + mylist[6] " Training!")
+print("\nHi " + name.capitalize() + "! Welcome to Day " + str(mylist[1]) + " of " + str(mylist[5]) + " Training!")
 
