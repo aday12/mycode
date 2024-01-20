57fcfc701413556524313688d3e817f5ab7acfce
aday12 <aaronday1996@icloud.com>
2024-01-20 20:01:17 +0000

    final commit, not perfect, not exactly what I want, but an MVP none the less

--- my_projects/week2_project/age.py
+++ my_projects/week2_project/age.py
@@ -22,19 +22,18 @@ def index():
 # def generation(age):
 #    return render_template("age.html", user_age=age)
 
-@app.route("/year/<bday>", methods = ["POST", "GET"])
-def year(bday):
-    api_url = 'https://api.api-ninjas.com/v1/historicalevents?text={}'.format(bday)
-    response = requests.get(api_url, headers={'X-Api-Key': 'QNYrADBFchwxcIyRQShmlA==JfMRdFvbWhHc5GFz'})
-    if response.status_code == requests.codes.ok:
-        return render_template("gen.html", year = response.text)
+@app.route("/year/<year>", methods = ["POST", "GET"])
+def year(year):
+    api_url = 'https://api.api-ninjas.com/v1/historicalevents?text={}'.format(year)
+    rsp = requests.get(api_url, headers={'X-Api-Key': 'QNYrADBFchwxcIyRQShmlA==JfMRdFvbWhHc5GFz'}).json()
+    fact = rsp[0]
+    return render_template("gen.html", year = fact)
 
 # this is the main function
 def main():
-    # fires the generation function from above
     name()
     generation()
     year()
-    # main only works if the user is inside of the main funciton
+# main only works if the user is inside of the main funciton
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port = 2224)
